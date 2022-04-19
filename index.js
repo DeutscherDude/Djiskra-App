3caac80c74d3ebe3c9d040d019ab29692555e8ed
DeutscherDude <kolokius@gmail.com>
2022-04-19 19:10:20 +0200

    Restructuring to have both backend & frontend separated

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -11,17 +13,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', function(req, res) {
+app.get('/pokemons', function(req, res) {
     var list = ['pikachu', 'charmander', 'bulbasaur'];
     res.json(list);
     console.log("Sent list of items");
 })
 
 app.get('*', (req, res) => {
-    res.sendFile( path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 })
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-
