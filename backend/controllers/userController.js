7b0de2ad0d9a1380e62e7874bbcc4600e4a7577c
DeutscherDude <kolokius@gmail.com>
2022-04-22 15:46:25 +0200

    Added password hashing and simplified checks

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,9 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const asyncHandler = require('express-async-handler');
-
 const User = require('../models/userModel');
+
+
 
 const getUsers = asyncHandler(async (req, res) => {
     const receivedUsers = await User.find();
@@ -8,8 +11,42 @@ const getUsers = asyncHandler(async (req, res) => {
 });
 
 const createUser = asyncHandler(async (req, res) => {
-    const user = await User.create(req.body);
-    res.status(201).json(user);
+    const { name, email, password, role } = req.body;
+
+    if (!name || !email || !password || !role) {
+        res.status(400)
+        throw new Error('Please fill in all fields')
+    }
+
+    const userExists = await User.findOne({email});
+
+    if(userExists){
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+        role
+    });
+
+    if(user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    }
+    else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 });
 
 const deleteUser = asyncHandler(async (req, res) => {
